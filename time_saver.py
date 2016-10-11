97ba5afa53264cd23ce55ab7bdc99a1f4f6978be
Steven Deshazer <steven.deshazer@gmail.com>
2016-10-10 19:48:37 -0600

    Final updates to comments and updated formatting.

--- time_saver.py
+++ time_saver.py
@@ -5,7 +5,9 @@ import re
 #   Reads the entire file into a string
 #   Processes the string to separate out the address into 4 columns
 #   Writes the output to the file name specified by the param outputfile
-#   Closing of the opened files should be handled outside the call to this method
+#   Closing of the opened files should be handled outside the call to this method,
+#   and the files should already be opened when passed to this method.
+
 def process_csv_separate_addr(inputfile, outputfile):
 
 
@@ -20,21 +22,17 @@ def process_csv_separate_addr(inputfile, outputfile):
     # NOTE: If you comment this line out the c/o case will have to be handled manually before processing
     output = re.sub(r"(\"c/o .+)(?<=[\w])[\r\n]+", r"\1 ", originaltext, flags=re.MULTILINE)
 
-    #print output   #debug print statement
-
     #find and replace address in one cell with address split into 4 cells:
     #Street Address, City, State, Zip Code
     output = re.sub(r"\"([^\"]+)\n(.+) (\w{2}) (\d{5})\"", r"\1,\2,\3,\4", output, flags=re.MULTILINE)
-
-    #print output   #debug print statement
 
     #remove \n from other cells
     # ---DOESN'T WORK YET---
     #output = re.sub(r"\"([^\"]+)\n([^\"]+)\"", r"\1 or \2", output, flags=re.MULTILINE)
 
-    #print output   #debug print statement
-
-
+    # Output the column headers separately
     outputfile.write("Order Number / RMA #,Serial Number,Date,Customer Name,Address,City,State,Zip,"
                     "Phone Number,Email Address,Special Instructions\n")
+
+    # Output the processed text
     outputfile.write(output)
